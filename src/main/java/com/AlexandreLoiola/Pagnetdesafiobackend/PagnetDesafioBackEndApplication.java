dea6960be4afd9eca164a14e3bdbd8b857371a02
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2024-05-06 22:23:29 -0300

    feat: Add Service

---
+++ src/main/java/com/AlexandreLoiola/Pagnetdesafiobackend/PagnetDesafioBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.AlexandreLoiola.Pagnetdesafiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagnetDesafioBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagnetDesafioBackEndApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/AlexandreLoiola/Pagnetdesafiobackend/service/TransactionService.java
@@ -0,0 +1,141 @@
+package com.AlexandreLoiola.Pagnetdesafiobackend.service;
+
+import com.AlexandreLoiola.Pagnetdesafiobackend.dto.TransactionDto;
+import com.AlexandreLoiola.Pagnetdesafiobackend.dto.TransactionTypeDto;
+import com.AlexandreLoiola.Pagnetdesafiobackend.model.TransactionModel;
+import com.AlexandreLoiola.Pagnetdesafiobackend.model.TransactionTypeModel;
+import com.AlexandreLoiola.Pagnetdesafiobackend.repository.TransactionRepository;
+import com.AlexandreLoiola.Pagnetdesafiobackend.repository.TransactionTypeRepository;
+import com.AlexandreLoiola.Pagnetdesafiobackend.service.exception.FileProcessingException;
+import com.AlexandreLoiola.Pagnetdesafiobackend.service.exception.InvalidAmountFormatException;
+import com.AlexandreLoiola.Pagnetdesafiobackend.service.exception.InvalidDateFormatException;
+import com.AlexandreLoiola.Pagnetdesafiobackend.service.exception.TransactionTypeNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class TransactionService {
+
+  private TransactionRepository transactionRepository;
+
+  private TransactionTypeRepository transactionTypeRepository;
+
+  public TransactionService(TransactionRepository transactionRepository, TransactionTypeRepository transactionTypeRepository) {
+    this.transactionRepository = transactionRepository;
+      this.transactionTypeRepository = transactionTypeRepository;
+  }
+
+  public Set<TransactionDto> processUploadedTransactions(MultipartFile file) {
+    Set<TransactionDto> transactionDtos = new HashSet<>();
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        TransactionModel transactionModel =  parseTxtFileToModel(line);
+        TransactionDto transactionDto = registerTransactions(transactionModel);
+        transactionDtos.add(transactionDto);
+      }
+    } catch (Exception e) {
+      throw new FileProcessingException("Error processing file");
+    }
+    return transactionDtos;
+  }
+
+  public Set<TransactionDto> getOperations() {
+    Set<TransactionModel> transactionModel = transactionRepository.findAllByIsActiveTrue();
+    return convertSetModelToSetDto(transactionModel);
+  }
+
+  private TransactionTypeModel findTransactionTypeModel(char type) {
+    return transactionTypeRepository.findByTypeAndIsActiveTrue(type).orElseThrow(() -> new RuntimeException("Cannot find the transaction type"));
+  }
+
+  @Transactional
+  public TransactionDto registerTransactions(TransactionModel transactionModel) {
+    transactionModel.setActive(true);
+    transactionRepository.save(transactionModel);
+    return convertModelToDto(transactionModel);
+  }
+
+  TransactionModel parseTxtFileToModel(String line) {
+    TransactionModel transactionModel = new TransactionModel();
+    try {
+      var transactionTypeModel = findTransactionTypeModel(line.charAt(0));
+      transactionModel.setTransactionType(transactionTypeModel);
+    } catch (Exception err) {
+      throw new TransactionTypeNotFoundException("Cannot find the transaction type");
+    }
+    try {
+      DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+      transactionModel.setDate(LocalDate.parse(line.substring(1, 9), dateFormatter));
+      DateTimeFormatter hourFormatter = DateTimeFormatter.ofPattern("HHmmss");
+      transactionModel.setHour(LocalTime.parse(line.substring(42, 48), hourFormatter));
+    } catch (DateTimeParseException e) {
+      throw new InvalidDateFormatException("Invalid date format");
+    }
+    try {
+      transactionModel.setAmount((Float.parseFloat(line.substring(10, 19)) / 100));
+    } catch (NumberFormatException e) {
+      throw new InvalidAmountFormatException("Invalid amount format");
+    }
+    transactionModel.setCpf(line.substring(20, 30));
+    transactionModel.setCard(line.substring(31, 42));
+    transactionModel.setOwnerStore(line.substring(48, 62));
+    transactionModel.setNameStore(line.substring(62));
+    return transactionModel;
+  }
+
+  TransactionDto convertModelToDto(TransactionModel transactionModel) {
+    if (transactionModel == null) {
+      throw new IllegalArgumentException("TransactionModel cannot be null");
+    }
+    var transactionDto = new TransactionDto();
+    try {
+      TransactionTypeModel transactionTypeModel = findTransactionTypeModel(transactionModel.getTransactionType().getType());
+      var transactionTypeDto = new TransactionTypeDto();
+      transactionTypeDto.setDescription(transactionTypeModel.getDescription());
+      transactionTypeDto.setFlowDirection(transactionTypeModel.getFlowDirection());
+      transactionDto.setType(transactionTypeDto);
+    } catch (Exception e) {
+      throw new TransactionTypeNotFoundException("Not Found Transaction Type");
+    }
+    try {
+      transactionDto.setDate(transactionModel.getDate());
+      transactionDto.setHour(transactionModel.getHour());
+    } catch (DateTimeException e) {
+      throw new InvalidDateFormatException("Invalid date or time in TransactionModel");
+    }
+    try {
+      transactionDto.setAmount(transactionModel.getAmount());
+    } catch (NumberFormatException e) {
+      throw new InvalidAmountFormatException("Invalid amount in TransactionModel");
+    }
+    transactionDto.setCpf(transactionModel.getCpf());
+    transactionDto.setCard(transactionModel.getCard());
+    transactionDto.setOwnerStore(transactionModel.getOwnerStore());
+    transactionDto.setNameStore(transactionModel.getNameStore());
+    return transactionDto;
+  }
+
+  Set<TransactionDto> convertSetModelToSetDto(Set<TransactionModel> transactionModels) {
+    if (transactionModels != null || transactionModels.isEmpty()) {
+      throw new IllegalArgumentException("TransactionModel cannot be null");
+    }
+    Set<TransactionDto> transactionDtos = new HashSet<>();
+    for (TransactionModel transactionModel : transactionModels) {
+      var transactionDto = convertModelToDto(transactionModel);
+      transactionDtos.add(transactionDto);
+    }
+    return transactionDtos;
+  }
+}
